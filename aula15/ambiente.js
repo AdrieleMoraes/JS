8b21ca5c5114cb672327ce14801bc14299b5ea98
DriMoraes <adrieledemoraes@yahoo.com.br>
2022-08-02 19:50:12 -0300

    Create ambiente.js
    aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,21 @@
+/*let num = [5,8,2,9,3]
+console.log(num)*/
+//console.log(`Nosso vetor é o ${num}`)
+
+
+/*let num = [5,8,4]
+
+num[3] = 6
+
+console.log(num)*/
+
+//console.clear() para limpar o terminal
+let num = [5,8,4]
+
+num.push(7)
+num.sort()
+//console.log(num)
+
+//console.log(num.length)//mostra o comprimento do array
+//num.sort//coloca os elementos em ordem crescente
+console.log(num)
