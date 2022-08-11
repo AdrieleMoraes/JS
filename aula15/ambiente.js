b2db49af19b5968a2e69947d70fcb8c6af748331
AdrieleMoraes <adrieledemoraes@yahoo.com.br>
2022-08-11 16:58:49 -0300

    alterações
    conclusão da aula 15 e exercícios

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -10,12 +10,22 @@ num[3] = 6
 console.log(num)*/
 
 //console.clear() para limpar o terminal
-let num = [5,8,4]
+let num = [5,8,2,9,3]
 
-num.push(7)
+num.push(1)
 num.sort()
+
 //console.log(num)
 
 //console.log(num.length)//mostra o comprimento do array
 //num.sort//coloca os elementos em ordem crescente
 console.log(num)
+
+//indexOf procura um valor dentro de um vetor
+let pos = num.indexOf(5)
+//console.log(pos) ou console.log(`O valor 8 está na posição ${pos}`)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+}else {
+    console.log(`O valor está na posição ${pos}`)
+}
